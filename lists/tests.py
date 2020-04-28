73880717cd1fe5db26988dee34011aa09561dac1
ZengHaonan <1114560730@qq.com>
2020-04-28 10:30:26 +0800

    Model for Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from lists.models import Item
+
 
 
 from lists.views import home_page 
@@ -20,17 +22,29 @@ class HomePageTest(TestCase):
         response=self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
 
+    def test_can_save_a_POST_request(self):
+        response=self.client.post('/',data={'item_text':'A new list item'})
+        self.assertIn('A new list item',response.content.decode())
+        self.assertTemplateUsed(response,'home.html')
 
-    #def test_home_page_returns_correct_html(self):
-       # request=HttpRequest()
-       # response=home_page(request)
-       
-       # response=self.client.get('/')#1
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item=Item()
+        first_item.text ='The first (ever) list item'
+        first_item.save()
 
-        
-        #html=response.content.decode('utf8')
-        #self.assertTrue(html.startswith('<html>'))
-        #self.assertIn('<title>To-Do lists</title>',html)
-        #self.assertTrue(html.strip().endswith('</html>'))
+        second_item=Item()
+        second_item.text ='Item the second'
+        second_item.save()
 
-        #self.assertTemplateUsed(response,'home.html')
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item=saved_items[0]
+        second_saved_item=saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+
+
--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
-
+from django.http import HttpResponse
 
 # Create your views here.
 #home_page = None
 def home_page(request):
-    return render(request,'home.html')
-
+    #return render(request,'home.html')
+    #if request.method=='POST':
+    #    return HttpResponse(request.POST['item_text'])
+    return render(request,'home.html',{
+        'new_item_text':request.POST.get('item_text',''),
+        })
