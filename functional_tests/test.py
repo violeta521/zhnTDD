5b345d33243770c16ebf7cac2b8dfca467e5a6ad
ZengHaonan <1114560730@qq.com>
2020-05-26 19:01:42 +0800

    set STATIC-ROOT in settings and disable admin

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -3,6 +3,7 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 #from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import os
 
 MAX_WAIT=10
 
@@ -13,6 +14,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://'+staging_server
 
     def tearDown(self):
         self.browser.quit()
